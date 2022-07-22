6665e1aae8ccb0eba860a3b1b28f8ad29d094834
brejnev1917 <brejnev1917@gmail.com>
2022-07-22 13:27:26 +0200

    added junit4 and test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -13,4 +13,13 @@ public class CashbackHackServiceTest {
         int expected = 100;
         assertEquals(actual, expected);
     }
+    @org.junit.Test
+    public void boughtExactly1000() {
+        CashbackHackService cashback = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = cashback.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
 }
