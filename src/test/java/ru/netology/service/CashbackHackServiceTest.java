6c383740bf32697493f3bde7c0826b986983b51a
brejnev1917 <brejnev1917@gmail.com>
2022-07-22 13:15:45 +0200

    added testng and test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -16,5 +16,12 @@ public class CashbackHackServiceTest {
         assertEquals(actual, expected);
 
     }
-
+    @org.testng.annotations.Test
+    public void boughtExactly1000() {
+        CashbackHackService cashback = new CashbackHackService();
+        int amount = 1000;
+        int actual = cashback.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
 }
